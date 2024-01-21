b887e47465b0baf21ef8da3d9d7dfd0c042971cd
Shubham <shubgoyal1998@gmail.com>
2024-01-21 18:50:59 +0530

    added strick mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import {
-  RouterProvider,
-   createBrowserRouter,
-} from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Layout from "./Layout.jsx";
 import { Home, About, Contact, ApiKey } from "./Components/index.js";
-import "./index.css"
+import "./index.css";
 
 const router = createBrowserRouter([
    {
@@ -16,11 +13,13 @@ const router = createBrowserRouter([
          { path: "", element: <Home /> },
          { path: "/about", element: <About /> },
          { path: "/contact", element: <Contact /> },
-         {path: "/apikey", element: <ApiKey />}
+         { path: "/apikey", element: <ApiKey /> },
       ],
    },
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-      <RouterProvider router={router}/>
+   <React.StrictMode>
+      <RouterProvider router={router} />
+   </React.StrictMode>
 );
